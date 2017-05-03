f69a227fe8ef104b04d7c2553b82e5699b97a967
Michael Tambornino <michael.tambornino@gmail.com>
2017-05-03 00:44:30 -0500

    added recursion

--- recursion-factorial.js
+++ recursion-factorial.js
@@ -4,9 +4,12 @@
 // is the product of all positive integers less than or equal to n.
 // Your task is to to create the function factorial that receives n and returns n!. You must use recursion.
 
-const recursion = (n) => {
-  return n === 1 ? 1 : n * recursion(n-1)
+const factorial = (n) => {
+  const recur = (x) => {
+    return x === 1 ? 1 : x * recur(x-1)
+  }
+  return recur(n)
 }
 
-console.log(recursion(1), `should equal 1`)
-console.log(recursion(5), `should equal 120`)
+console.log(factorial(1), `should equal 1`)
+console.log(factorial(5), `should equal 120`)
